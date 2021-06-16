87a39574b51707db0db2f2a13b2cc4918f75267c
Rahul Reddy <33981256+rahulcreddy@users.noreply.github.com>
2021-06-16 22:58:37 +0530

    Update script.py

--- script.py
+++ script.py
@@ -4,36 +4,20 @@ import matplotlib.pyplot as plt
 from pprint import pprint
 #from scipy.io.wavfile import write
 
-SAMPLE_RATE = 44100
-AMPLITUDE = 4096
-
-def get_wave(frequency,duration):
-    t = np.linspace(0,duration,int(SAMPLE_RATE*duration)) #Equally distributed time intervals to mimic a wave
-    wave = AMPLITUDE*np.sin(2*np.pi*frequency*t)
+def get_wave(frequency, duration, sample_rate = 44100, amplitude = 4096):
+    '''
+    Returns an array of sine wave of a particular frequency.
+    '''
+    t = np.linspace(0,duration,int(sample_rate*duration)) #Equally distributed time intervals to mimic a wave
+    wave = amplitude*np.sin(2*np.pi*frequency*t)
     return wave
-  
-#a_wave = get_wave(440,1)
-#print(len(a_wave))
-#type(a_wave)
-#plt.plot(a_wave[0:int(44100/440)])
-#plt.xlabel('time')
-#plt.ylabel('Amplitude')
-#plt.show()
 
-
-##################### SINGLE OCTAVE KEY NOTES OF PIANO #####################
-#def get_piano_notes():
-#    octave = ['C','c','D','d','E','F','f','G','g','A','a','B']
-#    base_freq = 523.25 #C5 note frequency
-    
-#    note_freqs = {octave[i]: base_freq*pow(2,(i/12)) for i in range(len(octave))}
-    
-#    note_freqs[''] = 0.0 #silent note
-    
-#    return note_freqs
-
-##################### 88 KEY NOTES OF PIANO #####################
 def get_piano_notes():
+    '''
+    Returns dict of frequencies of a 88 key standard piano.
+    Math equation for frequency calculation: 2^((n-49)/12)*440Hz where n is key number from A4 (440 Hz)
+    '''
+
     octave = ['C','c','D','d','E','F','f','G','g','A','a','B']
     base_freq = 440 #A4 frequency in Hz
 
@@ -43,62 +27,22 @@ def get_piano_notes():
     end_index = np.where(keys == 'C8')[0][0]
     keys = keys[start_index:end_index+1]
 
-    note_freqs = dict(zip(keys,[2**((n+1-49)/12)*base_freq for n in range(len(keys))]))
+    note_freqs = dict(zip(keys,[2**((n-49)/12)*base_freq for n in range(1,len(keys)+1)]))
     note_freqs[''] = 0.0
 
     return note_freqs
   
-note_freqs = get_piano_notes()
-#c4_freq = note_freqs['C4']
-#pprint(note_freqs)
-
-#notes = 'c+E+c+c+E+c+c+E+c+d+c+B' #Shape of You Melody
-
-## WRITE SONG
-
-#def get_song_data(music_notes):
-#   note_freqs = get_piano_notes()
-#   song = [get_wave(note_freqs[note]) for note in music_notes.split('+')]
-#   song = np.concatenate(song)
-#   return song
-  
-#data = get_song_data(notes)
-
-#write('shape_of_you_melody.wav', frequency, data.astype(np.int16))
-
-# ADSR Weights for Piano
-def get_adsr_weights(frequency, duration, length, decay, sustain_level,sample_rate=44100):
-
-    assert abs(sum(length)-1) < 1e-8
-    assert len(length) ==len(decay) == 4
+def apply_overtones(frequency, duration, factor = [0.67, 0.25, 0.02, 0.02, 0.04], sample_rate = 44100, amplitude = 4096):
+    '''
+    Returns a note with overtones applied.
+    Default is 4 overtones on the fundamental note.
+    '''
     
-    intervals = int(duration*frequency)
-    len_A = np.maximum(int(intervals*length[0]),1)
-    len_D = np.maximum(int(intervals*length[1]),1)
-    len_S = np.maximum(int(intervals*length[2]),1)
-    len_R = np.maximum(int(intervals*length[3]),1)
+    frequencies = np.minimum(np.array([frequency*(x) for x in range(1,len(factor)+1)]), sample_rate//3)
+    amplitudes = np.array([amplitude*x for x in factor])
     
-    decay_A = decay[0]
-    decay_D = decay[1]
-    decay_S = decay[2]
-    decay_R = decay[3]
-    
-    A = 1/np.array([(1-decay_A)**n for n in range(len_A)])
-    A = A/np.nanmax(A)
-    D = np.array([(1-decay_D)**n for n in range(len_D)])
-    D = D*(1-sustain_level)+sustain_level
-    S = np.array([(1-decay_S)**n for n in range(len_S)])
-    S = S*sustain_level
-    R = np.array([(1-decay_R)**n for n in range(len_R)])
-    R = R*S[-1]
-    
-    weights = np.concatenate((A,D,S,R))
-    smoothing = np.array([0.1*(1-0.1)**n for n in range(5)])
-    smoothing = smoothing/np.nansum(smoothing)
-    weights = np.convolve(weights, smoothing, mode='same')
-    
-    weights = np.repeat(weights, int(sample_rate*duration/intervals))
-    tail = int(sample_rate*duration-weights.shape[0])
-    if tail > 0:
-        weights = np.concatenate((weights, weights[-1]-weights[-1]/tail*np.arange(tail)))
-    return weights
+    fundamental = get_wave(frequencies[0], duration, sample_rate, amplitudes[0])
+    for i in range(1, len(factor)):
+        overtone = get_wave(frequencies[i], duration, sample_rate, amplitudes[i])
+        fundamental += overtone
+    return fundamental
